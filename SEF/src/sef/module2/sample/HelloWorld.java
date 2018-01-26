3fd852cf9671e1e80754c404de389759953f57ab
konstantin.pletenev <konstantin.pletenev@accenture.com>
2018-01-26 17:23:08 +0200

    test

--- SEF/src/sef/module2/sample/HelloWorld.java
+++ SEF/src/sef/module2/sample/HelloWorld.java
@@ -1,8 +1,9 @@
 package sef.module2.sample;
 
 public class HelloWorld {
-	
-	public static void main(String arg[]){
-		System.out.println("Hello World!");
-	}
+
+    public static void main(String arg[]) {
+        System.out.println("Hello Bootcampers! " +
+                "\nWelcome to the Java course!");
+    }
 }
